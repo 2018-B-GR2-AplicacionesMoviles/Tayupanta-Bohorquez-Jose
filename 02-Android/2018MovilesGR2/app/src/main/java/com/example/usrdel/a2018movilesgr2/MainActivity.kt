ceb7f1ced5071bc8066a81fb718b4044ef694cf7
josetabo21 <jose_btb@hotmail.com>
2018-11-15 14:48:26 -0500

    ASH

--- 02-Android/2018MovilesGR2/app/src/main/java/com/example/usrdel/a2018movilesgr2/MainActivity.kt
+++ 02-Android/2018MovilesGR2/app/src/main/java/com/example/usrdel/a2018movilesgr2/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.usrdel.a2018movilesgr2
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -31,6 +32,17 @@ class MainActivity : AppCompatActivity() {
         intentIrABotones.putExtra("nombre","Jose")
         intentIrABotones.putExtra("apellido", "Tayupanta")
         this.startActivity(intentIrABotones)
+    }
+
+    fun enviarIntentRespuesta(){
+        val intentRespuesta = Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.)
+        this.startActivityForResult(requestCode, resultCode,data)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Log.i("intente-respuesta","requestCode:"$requestCode)
     }
 
 }
